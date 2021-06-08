394bd7394fe1e1cc291796de573e4a251c7db389
mayuri <mayurishah0408@gmail.com>
2021-06-08 17:00:19 -0400

    Commit first

---
+++ routes/users.js
@@ -0,0 +1,93 @@
+ const Router = require("express").Router;
+ const User = require("../models/user");
+ const {ensureLoggedIn, ensureCorrectUser} = require("../middleware/auth");
+ const app = require("../app")
+ const router = new Router();
+ /** const nunjucks = require("nunjucks");
+ nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});**/
+
+ 
+ /** get list of users.
+  *
+  * => {users: [{username, first_name, last_name, phone}, ...]}
+  *
+  **/
+ 
+ router.get("/", ensureLoggedIn, async function (req, res, next) {
+   try {
+     let users = await User.all();
+
+     return res.json({users});
+      //res.render("users_all.html")
+   }
+ 
+   catch (err) {
+     return next(err);
+   }
+ });
+ 
+ /** get detail of users.
+  *
+  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
+  *
+  **/
+ 
+ router.get("/:username", ensureCorrectUser, async function (req, res, next) {
+   try {
+     let user = await User.get(req.params.username);
+     return res.json({user});
+   }
+ 
+   catch (err) {
+     return next(err);
+   }
+ });
+ 
+ /** get messages to user
+  *
+  * => {messages: [{id,
+  *                 body,
+  *                 sent_at,
+  *                 read_at,
+  *                 from_user: {username, first_name, last_name, phone}}, ...]}
+  *
+  **/
+ 
+ router.get("/:username/to", ensureCorrectUser, async function (req, res, next) {
+   try {
+     let messages = await User.messagesTo(req.params.username);
+     return res.json({messages});
+   }
+ 
+   catch (err) {
+     return next(err);
+   }
+ });
+ 
+ /** get messages from user
+  *
+  * => {messages: [{id,
+  *                 body,
+  *                 sent_at,
+  *                 read_at,
+  *                 to_user: {username, first_name, last_name, phone}}, ...]}
+  *
+  **/
+ 
+ router.get("/:username/from", ensureCorrectUser, async function (req, res, next) {
+   try {
+     let messages = await User.messagesFrom(req.params.username);
+     return res.json({messages});
+   }
+ 
+   catch (err) {
+     return next(err);
+   }
+ });
+ 
+ 
+ 
+ module.exports = router;
